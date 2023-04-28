b8a578a4cef99809f2bb3b867a062d967b9d8fcf
Tin Tran <tranphamminhtin@gmail.com>
2023-04-28 22:16:46 +0700

    lesson-10-dom

---
+++ lesson-10-dom/script.js
@@ -0,0 +1,57 @@
+/* 
+  querySelector 
+  getElementById
+  getElementsByTagName
+  getElementsByClassName
+  querySelectorAll
+*/
+
+const h2Tag = document.getElementById("titleh2");
+h2Tag.innerHTML = "updated";
+
+const input = document.getElementById("input"); // lấy element
+input.value = "input changed";
+input.style.backgroundColor = "red";
+
+function hello(event) {
+  // console.log(event);
+  console.log(event.target.id); // lấy id của thẻ button
+  // alert("hello");
+}
+
+const btn = document.getElementById("btn");
+btn.onclick = hello;
+
+function ThemSanPham(product) {
+  const productListElement = document.getElementById("product-list");
+  const item = document.createElement("li");
+  item.id = product.id;
+  item.onclick = hello;
+  item.innerHTML = product.name;
+  productListElement.appendChild(item); // removeChild
+}
+
+function ThemSanPham2(product) {
+  const productListElement = document.getElementById("product-list");
+
+  const item = `<li>${product}</li>`;
+  // const item = "<li>" + product + "</li>"; // <li>Water</li>
+  productListElement.innerHTML += item;
+}
+
+// ThemSanPham();
+
+const products = [
+  { name: "Coffee", id: "cf" }, //0
+  { name: "Tea", id: "tea" }, // 1
+  { name: "Milk", id: "m" }, // 2
+  { name: "Water", id: "w" },
+  { name: "Ice-cream", id: "i" },
+];
+
+function TaoListSanPham(productList) {
+  for (const p of productList) {
+    ThemSanPham(p); // { name: "Coffee", id: "cf" }
+  }
+}
+TaoListSanPham(products);
